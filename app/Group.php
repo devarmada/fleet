241fc9bc9f99a7f02da366f38924eb4ae6750368
Andrea De Rinaldis <andrea@derinaldis.com>
2016-10-29 08:52:05 +0200

    implemented group model

--- app/Group.php
+++ app/Group.php
@@ -6,6 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Group extends Model {
 
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name',
+    ];
+
     public function users() {
         return $this->belongsToMany('App\User');
     }
