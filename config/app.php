c5b1b8909471ac85b7442f2ae4e2195d57466892
Andrea De Rinaldis <andrea@derinaldis.com>
2016-11-01 11:33:25 +0100

    renamed app to Fleet.aero - adjusted some app parameters

--- config/app.php
+++ config/app.php
@@ -12,7 +12,7 @@ return [
       | any other location as required by the application or its packages.
      */
 
-    'name' => 'Fleet.air',
+    'name' => 'Fleet.aero',
     /*
       |--------------------------------------------------------------------------
       | Application Environment
